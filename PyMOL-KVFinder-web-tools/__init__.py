def36047bf4486be3cd9108804ef2262f378d7f9
jvsguerra <jvsguerra@gmail.com>
2023-02-27 16:56:20 -0300

    Update pymol plugin

--- PyMOL-KVFinder-web-tools/__init__.py
+++ PyMOL-KVFinder-web-tools/__init__.py
@@ -1911,6 +1911,9 @@ class PyMOLKVFinderWebTools(QMainWindow):
         # Refresh residues
         self.refresh_residues()
 
+        # Set default view in results
+        self.default_view.setChecked(True)
+
         # Load files as PyMOL objects
         cmd.delete("cavities")
         cmd.delete("residues")
